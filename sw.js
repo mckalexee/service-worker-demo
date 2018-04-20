e35b19c9d086f6ca207eddf4d9622cbc279fe114
Alex McKenzie <mckenzie@tailwindapp.com>
2018-04-20 11:06:09 -0500

    add communicaton from sw

--- sw.js
+++ sw.js
@@ -12,3 +12,11 @@ self.addEventListener('message', function(event) {
     self.skipWaiting();
   }
 });
+
+function send_message_to_all_clients(msg){
+  clients.matchAll().then(clients => {
+      clients.forEach(client => {
+          send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
+      })
+  })
+}
